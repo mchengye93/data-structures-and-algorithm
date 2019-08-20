37009f200736686e2cdcff2c28f5f821d6732615
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-20 16:58:19 -0700

    oneAway test cases

--- JavaScript/chapter01/1.5 - OneAway/mchengye93_sol.js
+++ JavaScript/chapter01/1.5 - OneAway/mchengye93_sol.js
@@ -1,5 +1,5 @@
-let oneAway = (word1, word2) => {
-    let changes = 0;
+let isOneAway = (word1, word2) => {
+   
     if (word1.length > word2.length) {
         for (let i  = 0; i < word2.length; i++) {
             if (word2[i] !== word1[i]) {
@@ -20,6 +20,56 @@ let oneAway = (word1, word2) => {
             }
         }
     } 
+    let changes = 0;
+    if (word1.length === word2.length) {
+        for (let i = 0; i < word1.length; i++) {
+            if (word1[i] !== word2[i]) {
+                changes++;
+            }
+            if (changes > 1) {
+                return false;
+            }
+        }
+    }
+    return true;
 
 
 }
+
+
+// ****** TESTS ******
+function runTests(cases, expected) {
+    for (const [str1, str2] of cases) {
+      console.log(
+        isOneAway(str1, str2) === expected && isOneAway(str2, str1) === expected
+      );
+    }
+  }
+  
+  runTests(
+    [
+      ['pale', 'ple'], // deletion
+      ['pale', 'opale'], // insertion in beginning
+      ['pale', 'palse'], // insertion in middle
+      ['pale', 'pales'], // insertion at end
+      ['pale', 'bale'], // replacement
+      ['p', 'b'],
+      ['p', 'p'],
+      ['p', ''],
+      ['', '']
+    ],
+    true
+  );
+  
+  runTests(
+    [
+      ['pale', 'ae'], // greater than 1 deletions
+      ['pale', 'ppalpe'], // greater than 1 insertions
+      ['pale', 'bake'], // greater than 1 replacements
+      ['pale', 'balpe'], // 1 insertion, 1 replacement
+      ['pale', 'plo'], // 1 deletion, 1 replacement
+      ['pale', 'ales'] // 1 deletion, 1 insertion
+    ],
+    false
+  );
+  
