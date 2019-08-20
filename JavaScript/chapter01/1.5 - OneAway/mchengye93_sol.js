bd4b7a39892129490b5094052532ccaf62a1d585
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-20 16:56:28 -0700

    oneAway MVP

--- JavaScript/chapter01/1.5 - OneAway/mchengye93_sol.js
+++ JavaScript/chapter01/1.5 - OneAway/mchengye93_sol.js
@@ -1,3 +1,25 @@
 let oneAway = (word1, word2) => {
-    
+    let changes = 0;
+    if (word1.length > word2.length) {
+        for (let i  = 0; i < word2.length; i++) {
+            if (word2[i] !== word1[i]) {
+                //check if next substring is same
+                if (word1.substring(i+1) !== word2.substring(i)) {
+                    return false;
+                }
+            }
+        }
+    } 
+    if (word2.length > word1.length) {
+        for (let i  = 0; i < word1.length; i++) {
+            if (word1[i] !== word2[i]) {
+                //check if next substring is same
+                if (word2.substring(i+1) !== word1.substring(i)) {
+                    return false;
+                }
+            }
+        }
+    } 
+
+
 }
