acf4ac04bc2069d2618acc2fb42a0fe4f17a760c
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-20 23:01:10 -0700

    string compression problem

--- JavaScript/chapter01/1.6 - String Compression/mchengye93_sol.js
+++ JavaScript/chapter01/1.6 - String Compression/mchengye93_sol.js
@@ -1,3 +1,20 @@
 let stringCompression = (string) => {
+    let count = 1;
+    let compresStr = '';
+
+    let letter = string[0];
     
+    for (let i = 1; i < string.length; i++) {
+        let nextLetter = string[i];
+        if (letter !== nextLetter) {
+            compresStr += compresStr+letter+count;
+            count = 1;
+            letter = nextLetter;
+        } else {
+            count++;
+        }
+    }
+    return compresStr;
+
+
 }
