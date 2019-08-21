f2e251d5d580a6255697cf97ab3e82d3bdb48987
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-21 16:07:38 -0700

    Attempt zero matrix problem

--- JavaScript/chapter01/1.8 - Zero Matrix/mchengye93_sol.js
+++ JavaScript/chapter01/1.8 - Zero Matrix/mchengye93_sol.js
@@ -1,3 +1,19 @@
 let zeroMatrix = (matrix) => {
+
+    //go through each element in matrix
+    for (let r = 0; r < matrix.length; r++) {
+        
+        for (let c = 0; c < matrix[0].length; c++) {
+
+             //if you spot a zero make entire row zero
+            if (matrix[r][c] === 0) {
+                for (let i = 0; i < matrix[0].length; i++) {
+                    matrix[r][i] = 0;
+                }
+            }
+
+        }
+    }
+       
     
 }
