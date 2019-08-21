a3d8f9c889697a7058d7be1c41730b93df3ae399
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-21 16:02:42 -0700

    Rotate matrix mvp

--- JavaScript/chapter01/1.7 - Rotate Matrix/mchengye93_sol.js
+++ JavaScript/chapter01/1.7 - Rotate Matrix/mchengye93_sol.js
@@ -1,3 +1,14 @@
-let rotateMatrix = (matrix) => {
-    
-}
+let rotate = (matrix)=> {
+    const n = matrix.length;
+    const x = Math.floor(n/ 2);
+    const y = n - 1;
+    for (let i = 0; i < x; i++) {
+       for (let j = i; j < y - i; j++) {
+          k = matrix[i][j];
+          matrix[i][j] = matrix[y - j][i];
+          matrix[y - j][i] = matrix[y - i][y - j];
+          matrix[y - i][y - j] = matrix[j][y - i]
+          matrix[j][y - i] = k
+       }
+    }
+  }
