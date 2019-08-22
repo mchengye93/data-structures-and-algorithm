605ca6710141e4f54f7fb570d3e1d2ad873c06d7
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-22 13:22:27 -0700

    sum linkedlist reverse order attempt

--- JavaScript/chapter02/2.3 - Delete Middle Node/mchengye93_sol.js
+++ JavaScript/chapter02/2.3 - Delete Middle Node/mchengye93_sol.js
@@ -2,8 +2,7 @@ let deleteMiddleNode = (head) => {
     let middle = head;
     let aheadNode = head;
 
-    let n = 0;
-    let node = head;
+   
 
     while (aheadNode.next.next !== null) {
         prev = middle;
@@ -12,6 +11,6 @@ let deleteMiddleNode = (head) => {
         n++;
     }
     //this node is one behind middle so change it's next to skip middle
-    prev.next = prev.next.next;
+    prev.next = middle.next;
 
 }
---
+++ JavaScript/chapter02/2.5 - Sum Lists/mchengye93_sol.js
@@ -0,0 +1,28 @@
+let sumList = (node1, node2) => {
+    //reverseOrder
+    let input1 = '';
+    let input2 = '';
+
+    let input1Node = node1;
+    while (input1Node.next  !== null) {
+        input1 = input1Node.val + input1;
+        input1Node = input1Node.next;
+    }
+
+    let input2Node = node2;
+    while (input2Node.next !== null) {
+        input2 = input2Node.val + input2;
+        input2Node = input2Node.next;
+    }
+
+    let sum = parseInt(input1) + parseInt(input2);
+
+    let sumArr = sum.toString().split('');
+
+    let node = new Node(sumArr[sumArr.length-1]);
+
+    for (let i = sumArr.length-2; i >= 0; i--) {
+        node.next = new Node(sumArr[i]);
+    }
+    return node;
+}
