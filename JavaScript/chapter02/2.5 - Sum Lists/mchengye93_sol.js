db33cdaf1640f433098e94f7d1ffc391f8c971df
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-22 20:20:38 -0700

    Tested sumlist linkedlist reversed

--- JavaScript/chapter02/2.5 - Sum Lists/mchengye93_sol.js
+++ JavaScript/chapter02/2.5 - Sum Lists/mchengye93_sol.js
@@ -13,25 +13,29 @@ let sumList = (node1, node2) => {
     let input2 = '';
 
     let input1Node = node1;
-    while (input1Node.next  !== null) {
+    while (input1Node !== null) {
         input1 = input1Node.val + input1;
         input1Node = input1Node.next;
     }
 
     let input2Node = node2;
-    while (input2Node.next !== null) {
+    while (input2Node !== null) {
         input2 = input2Node.val + input2;
         input2Node = input2Node.next;
     }
-
+    console.log(input1, input2);
     let sum = parseInt(input1) + parseInt(input2);
-
+    console.log(sum);
     let sumArr = sum.toString().split('');
-
+    console.log(sumArr);
     let node = new Node(sumArr[sumArr.length-1]);
 
+    let prev = node;
     for (let i = sumArr.length-2; i >= 0; i--) {
-        node.next = new Node(sumArr[i]);
+       let newNode = new Node(sumArr[i]);
+        prev.next = newNode;
+        prev = newNode;
+       
     }
     return node;
 }
@@ -48,4 +52,4 @@ let node5 = new Node(5);
 node4.next = node5;
 
 
-console.log(sumList(node, node4));
+console.log(sumList(node, node4)); // 375 reversed 5->7->3
