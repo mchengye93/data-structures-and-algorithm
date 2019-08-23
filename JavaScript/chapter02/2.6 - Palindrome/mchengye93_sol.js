9d37d1c5faecec9b61df5b3db3cf0c990471558f
Marbo Cheng Ye <marbochengye@gmail.com>
2019-08-22 20:03:21 -0700

    reverselinkedlist

--- JavaScript/chapter02/2.6 - Palindrome/mchengye93_sol.js
+++ JavaScript/chapter02/2.6 - Palindrome/mchengye93_sol.js
@@ -1,3 +1,24 @@
+class Node { 
+    // constructor 
+    constructor(data) 
+    { 
+        this.data = data; 
+        this.next = null
+    } 
+} 
 let palindrome = (node) => {
-    
+
 }
+
+let reverseLinkedList = (node) => {
+    let head = null;
+
+    while(node !== null) {
+        let newNode = new Node(node.data);
+        newNode.next = head;
+        head = newNode;
+        node = node.next;
+    }
+    return head;
+}
+
